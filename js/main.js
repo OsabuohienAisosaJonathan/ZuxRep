968296dec87f0ddd68fdd1de29c2a74c28f44d79
ZUXA <86187171+OsabuohienAisosaJonathan@users.noreply.github.com>
2024-02-04 15:07:00 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+  $('.flexslider').flexslider({
+		prevText: '',
+		nextText: ''
+	});
+
+  $('.testimonails-slider').flexslider({
+    animation: 'slide',
+    slideshowSpeed: 5000,
+    prevText: '',
+    nextText: '',
+    controlNav: false
+  });
+
+  $(function(){
+
+  // Instantiate MixItUp:
+
+  $('#Container').mixItUp();
+
+  
+
+  $(document).ready(function() {
+      $(".fancybox").fancybox();
+    });
+
+  });
+
+
+});
+
